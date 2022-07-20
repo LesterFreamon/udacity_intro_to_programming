18ac5dd984067f9e1335e6a8e59557e12f846ab9
Adam Arnon <adamarnon@Adams-MBP.attlocal.net>
2022-07-20 09:46:29 -0700

    everything

---
+++ project_4_shopping_cart/exp_file.js
@@ -0,0 +1,16 @@
+let price = 15.00;
+let money = 20.00;
+if (money >= price) {
+    console.log('buy the hammer')
+} else {
+    console.log('Do not buy the hammer')
+}
+
+// declares the sayHello function
+function sayHello() {
+  const message = "Hello!"
+  return message; // returns value instead of printing it
+}
+
+// function returns "Hello!" and console.log prints the return value
+console.log(sayHello());
